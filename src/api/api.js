55e506e4fe0dcccf46fde9353aa4d8ffbc18a654
Robin Mukanganise <seechrobin@gmail.com>
2019-01-06 22:27:35 +0000

    Added api call for Pros

--- src/api/api.js
+++ src/api/api.js
@@ -1,11 +1,31 @@
 import axios from "axios";
 
-import { API_URL } from "../constants/constants";
+import { API_URL, PAGINATION } from "../constants/constants";
 
 export default class api {
   getlistOfCategories = async () => {
     try {
       return await axios.get(`${API_URL.categories}`);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  getlistOfPros = async (category_id, location, pageOffset) => {
+    console.log(category_id, location, pageOffset);
+
+    const headers = {
+      "x-pagination-limit": PAGINATION.x_pagination_limit,
+      "X-pagination-offset": pageOffset
+    };
+
+    const data = {
+      category_id: category_id,
+      location: location
+    };
+
+    try {
+      return await axios.post(`${API_URL.pros}`, data, { headers: headers });
     } catch (error) {
       console.error(error);
     }
